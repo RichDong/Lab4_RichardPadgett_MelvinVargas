51ab57bcf70d36c53fbd0b9714d0cb64c51d2939
Melvin <melvinrenevargas19@gmail.com>
2017-11-03 13:45:39 -0600

    creacion del menu

--- Lab3_MelvinVargas_RichardPadgett/src/lab3_melvinvargas_richardpadgett/Lab3_MelvinVargas_RichardPadgett.java
+++ Lab3_MelvinVargas_RichardPadgett/src/lab3_melvinvargas_richardpadgett/Lab3_MelvinVargas_RichardPadgett.java
@@ -1,11 +1,43 @@
 
 package lab3_melvinvargas_richardpadgett;
 
+import java.util.ArrayList;
+
 public class Lab3_MelvinVargas_RichardPadgett {
 
-
+    static ArrayList<Guerreros>lista=new ArrayList();
     public static void main(String[] args) {
-        
+        String opcion="";
+        while(!opcion.equals("g")){
+            System.out.println("a -creacion de guerreros");
+            System.out.println("b -eliminacion de guereros");
+            System.out.println("c -registro de jugadores");
+            System.out.println("d-iniciar partida");
+            System.out.println("e-listar jugadores");
+            System.out.println("f-listar guerreros");
+            System.out.println("g-salir del programa");
+            if(opcion.equals("a")){
+                
+            }
+            if(opcion.equals("b")){
+                
+            }
+            if(opcion.equals("c")){
+                
+            }
+            if(opcion.equals("d")){
+                
+            }
+            if(opcion.equals("e")){
+                
+            }
+            if(opcion.equals("f")){
+                
+            }
+            if(opcion.equals("g")){
+                
+            }
+        }
     }
     
 }
